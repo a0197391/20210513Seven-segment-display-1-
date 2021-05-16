1a6bb629a3c190069ec0d576526f294129c847e6
a0197391 <83972769+a0197391@users.noreply.github.com>
2021-05-16 15:58:51 +0800

    Add files via upload

---
+++ Seven segment display+1-no_button.ino
@@ -0,0 +1,46 @@
+byte seven_seg_digits[10][7]={
+{0,0,0,0,0,0,1},
+{1,0,0,1,1,1,1},
+{0,0,1,0,0,1,0},
+{0,0,0,0,1,1,0},
+{1,0,0,1,1,0,0},
+{0,1,0,0,1,0,0},
+{0,1,0,0,0,0,0},
+{0,0,0,1,1,1,1},
+{0,0,0,0,0,0,0},
+{0,0,0,1,1,0,0},
+};
+
+byte pin_number[7]={2,3,4,5,6,7,8};
+
+
+int num=0;
+
+void setup() {
+  for(byte pin=0;pin<7;pin++){
+    pinMode(pin_number[pin],OUTPUT);
+  }
+  digitalWrite(9,0);
+ 
+
+  pinMode(12,INPUT_PULLUP);
+}
+
+void sevenSegWrite(byte digit){
+  byte pin=2;
+  for(byte seg=0;seg<7;seg++){
+    digitalWrite(pin,seven_seg_digits[digit][seg]);
+    pin++;
+    }
+}
+void loop() {
+ 
+num++;
+if(num>9){
+  num=0;
+
+ }
+ sevenSegWrite(num);
+ delay(1000);
+}
+
