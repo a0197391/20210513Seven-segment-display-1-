dcca9381dc742c8b07bb52d75e94e91c31fc5867
a0197391 <83972769+a0197391@users.noreply.github.com>
2021-05-19 21:38:48 +0800

    Update Seven segment display+1-need_button.ino

--- Seven segment display+1-need_button.ino
+++ Seven segment display+1-need_button.ino
@@ -1,48 +1,37 @@
-byte seven_seg_digits[10][7]={
-{0,0,0,0,0,0,1},
-{1,0,0,1,1,1,1},
+byte sevenseg[10][7]={{0,0,0,0,0,0,1},
+{0,1,0,1,1,1,1},
 {0,0,1,0,0,1,0},
 {0,0,0,0,1,1,0},
-{1,0,0,1,1,0,0},
-{0,1,0,0,1,0,0},
-{0,1,0,0,0,0,0},
+{0,1,0,1,1,0,0},
+{1,0,0,0,1,0,0},
+{1,0,0,0,0,0,0},
 {0,0,0,1,1,1,1},
 {0,0,0,0,0,0,0},
-{0,0,0,1,1,0,0},
-};
-
-byte pin_number[7]={2,3,4,5,6,7,8};
-
-const int buttonPin=12;
-int buttonState=0;
-int num=0;
-
+{0,0,0,1,1,0,0}};
+byte num[7]={2,3,4,5,6,7,8};
+const int buttonPin=12;   
+int presentButtonState;
+int i,vv=0,a=1;                                                                                                              
 void setup() {
-  for(byte pin=0;pin<7;pin++){
-    pinMode(pin_number[pin],OUTPUT);
+  // put your setup code here, to run once:
+  for(i=0;i<7;i++){
+pinMode(num[i],OUTPUT);
   }
-  digitalWrite(9,0);
-  digitalWrite(buttonPin,HIGH);
   pinMode(buttonPin,INPUT);
-  pinMode(12,INPUT_PULLUP);
+  digitalWrite(buttonPin,HIGH);
+}
+void sevensegwrite(byte n){
+for(i=0;i<7;i++){
+  digitalWrite(num[i],sevenseg[n][i]);
+  }
 }
 
-void sevenSegWrite(byte digit){
-  byte pin=2;
-  for(byte seg=0;seg<7;seg++){
-    digitalWrite(pin,seven_seg_digits[digit][seg]);
-    pin++;
-    }
-}
 void loop() {
- buttonState=digitalRead(buttonPin);
- if(buttonState==LOW){
-num++;
-if(num>9){
-  num=0;
+  // put your main code here, to run repeatedly:
+  presentButtonState =digitalRead(buttonPin);
+if( presentButtonState==0&&a==1){vv++;a=0;}
+if( presentButtonState==1){a=1;}
+  if(vv>9){vv=0;}
+  sevensegwrite(vv);
+delay(2);
 }
- }
- sevenSegWrite(num);
- delay(1000);
-}
-
